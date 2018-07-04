f3ef32566a11b6f9ea6bcba189fa8cd15e548e1f
Averi Johnson <averimj@live.com>
2018-07-03 21:11:30 -0600

    add more round methods

--- lib/round.rb
+++ lib/round.rb
@@ -9,17 +9,19 @@ class Round
                 :record_guess,
                 :feedback,
                 :number_correct,
-                :guesses_last_feedback,
                 :percent_correct
 
   def initialize(deck)
     @deck = deck
-    @guesses = guesses
+    @guesses = []
     @current_card = current_card
     @record_guess = record_guess
     @feedback = feedback
     @number_correct = number_correct
-    @guesses_last_feedback = guesses_last_feedback
     @percent_correct = percent_correct
+  end
+
+  def current_card
+    @deck.cards.first
   end
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -14,4 +14,28 @@ class RoundTest < Minitest::Test
     round = Round.new(deck)
     assert_instance_of Round, round
   end
+
+  def test_it_has_deck
+    card_1 = Card.new("3", "Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_instance_of Deck, round.deck
+  end
+
+  def test_it_can_guess
+    card_1 = Card.new("3", "Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal [], round.guesses
+  end
+
+  def test_it_current_card
+    card_1 = Card.new("3", "Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal card_1, round.current_card
+  end
 end
