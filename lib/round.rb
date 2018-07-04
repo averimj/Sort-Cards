8881ddfc9c41c8722124b1433271bf0080fa7671
Averi Johnson <averimj@live.com>
2018-07-04 15:05:37 -0600

    add more round features

--- lib/round.rb
+++ lib/round.rb
@@ -9,19 +9,32 @@ class Round
                 :record_guess,
                 :feedback,
                 :number_correct,
-                :percent_correct
+                :percent_correct,
+                :round
 
   def initialize(deck)
     @deck = deck
     @guesses = []
     @current_card = current_card
-    @record_guess = record_guess
     @feedback = feedback
     @number_correct = number_correct
     @percent_correct = percent_correct
+    @round = round
   end
 
   def current_card
     @deck.cards.first
   end
+
+  def record_guess(hash)
+  guess_string = "#{hash[:value]} of #{hash[:suit]}"
+  guess = Guess.new(guess_string, current_card)
+  @guesses << guess
+  guess
+  end
+
+  def count_guesses
+    @round.guesses.count
+  end
+  
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -38,4 +38,39 @@ class RoundTest < Minitest::Test
     round = Round.new(deck)
     assert_equal card_1, round.current_card
   end
+
+  def test_it_can_record_guess
+    card_1 = Card.new("3", "Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_instance_of Guess, round.record_guess({value: "3", suit: "Hearts"})
+  end
+
+  def test_it_can_count_guesses
+    card_1 = Card.new("3", "Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    round.record_guess({value: "3", suit: "Hearts"})
+    assert_equal 1, round.guesses.count
+  end
+
+  def test_it_gives_correct_feedback
+    card_1 = Card.new("3", "Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    round.record_guess({value: "3", suit: "Hearts"})
+    assert_equal "Correct!", round.guesses.first.feedback
+  end
+
+  # def test_it_is_correct_number
+  #   card_1 = Card.new("3", "Hearts")
+  #   card_2 = Card.new("4", "Clubs")
+  #   deck = Deck.new([card_1, card_2])
+  #   round = Round.new(deck)
+  #   # round.record_guess({value: "3", suit: "Hearts"})
+  #   assert_equal 1, round.number_correct
+  # end
 end
